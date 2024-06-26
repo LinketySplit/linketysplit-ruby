caa80b180b5541979cdc3d87f9d4ac8218881fc1
Christopher Carson <chris@nowzoo.com>
2024-06-26 14:16:18 -0500

    oh boy i am glad i do javascript

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 gemspec
 
+gem "jwt", "~> 2.8.2"
 gem "minitest", "~> 5.11"
 gem "minitest-rg", "~> 5.3"
 gem "rake", "~> 13.0"
@@ -9,4 +10,3 @@ gem "rubocop-minitest", "0.35.0"
 gem "rubocop-packaging", "0.5.2"
 gem "rubocop-performance", "1.21.0"
 gem "rubocop-rake", "0.6.0"
-gem "jwt", "~> 2.8.2"
--- lib/linketysplit/publication_sdk.rb
+++ lib/linketysplit/publication_sdk.rb
@@ -5,6 +5,7 @@ require "json"
 module Linketysplit
   class PublicationSdk
     attr_reader :api_key
+
     def initialize(api_key)
       @api_key = api_key
     end
@@ -12,35 +13,34 @@ module Linketysplit
     # Creates a signed article purchase link with the given permalink.
     #
     # @param permalink [String] The canonical URL of the article.
-    # @param customPricing [Hash, nil] Optional. Custom pricing to be applied for this purchase link -- that is, for a particular user.
+    # @param customPricing [Hash, nil] Optional. Custom pricing to be applied for this purchase link,
+    #   that is, for a particular user.
     #   - `:price` [Integer] The base article price in US cents.
     #   - `:discounts` [Array<Hash>] Optional. Quantity discounts.
     #     - `:quantity` [Integer] The minimum quantity at which the discount applies.
     #     - `:price` [Integer] The unit price of the article at the given quantity.
     # @param context [String, nil] Optional. Pass "sharing" if the user is sharing an article.
     # @return [String] The URL of the purchase page on LinketySplit. Show the reader a link to this URL.
-    def createArticlePurchaseLink(permalink, customPricing = nil, context = nil)
+    def create_article_purchase_link(permalink, custom_pricing=nil, context=nil)
       payload = {
-        permalink: permalink
+        permalink:
       }
 
-      if customPricing
-        payload[:customPricing] = customPricing
-      end
+      payload[:customPricing] = custom_pricing if custom_pricing
 
-      if context
-        payload[:context] = context
-      end
-      token = JWT.encode payload, @api_key, 'HS256'
-      return "https://linketysplit.com/purchase-link/#{token}"
+      payload[:context] = context if context
+      token = JWT.encode payload, @api_key, "HS256"
+      "https://linketysplit.com/purchase-link/#{token}"
     end
-    def self.getLinketySplitPricingMetaTag(articlePricing)
-      content = Base64.encode64(JSON.dump(articlePricing))
-      return "<meta property=\"linketysplit:pricing\" content=\"#{content}\" />"
+
+    def self.get_linketysplit_pricing_meta_tag(article_pricing)
+      content = Base64.encode64(JSON.dump(article_pricing))
+      "<meta property=\"linketysplit:pricing\" content=\"#{content}\" />"
     end
-    def self.getLinketySplitEnabledMetaTag(enabled = true)
+
+    def self.get_linketysplit_enabled_meta_tag(enabled: true)
       content = enabled ? "true" : "false"
-      return "<meta property=\"linketysplit:enabled\" content=\"#{content}\" />"
+      "<meta property=\"linketysplit:enabled\" content=\"#{content}\" />"
     end
   end
 end
